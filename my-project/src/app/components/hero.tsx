938c5a9a1de63a9d6c4f84e6228a58d6b1927925
hustle2live <kuz.v@ukr.net>
2025-02-23 16:50:54 +0200

    change hero button route

--- my-project/src/app/components/hero.tsx
+++ my-project/src/app/components/hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
                   <button
                      className='rounded-lg border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
                      onClick={() => {
-                        router.push('/sign-up');
+                        router.push('/documents-formation');
                      }}
                   >
                      Розпочати
